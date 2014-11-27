4eef6a33c9ee410a2238fd768505e58f898bd215
b695 <b695@borqs.com>
2014-11-27 11:38:03 +0800

    b695: fix bug 61606.
    Change-Id: I472a768f4c7442946b97d4f8982066b9f9f9cde3
    Reviewed-on: http://192.168.1.79/gerrit/9540
    Reviewed-by: b643 <b643@borqs.com>

--- drivers/gpu/drm/i915/intel_panel.c
+++ drivers/gpu/drm/i915/intel_panel.c
@@ -580,7 +580,9 @@ void intel_panel_actually_set_backlight(struct drm_device *dev, u32 level)
 	}
 #ifdef ENABLE_SIO_PWM
 	if (BYT_CR_CONFIG) {
-		level = level * 0xfd / 255 + 50;
+		level = level  + 50;
+		if(level > 0xff)
+			level = 0xff;
 		lpio_bl_write_bits(0, LPIO_PWM_CTRL,(0xFF-level), 0xFF);
 		lpio_bl_update(0, LPIO_PWM_CTRL);
 	} else 
