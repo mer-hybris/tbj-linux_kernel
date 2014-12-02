d2adf8278e3d71b47d2505cd1a1c36a475c963cb
b199 <b199@borqs.com>
2014-12-02 16:22:39 +0800

    Revert "drm/i915: Remove incorrect error msg from atomic display update path"
    This reverts commit ba81c7369c6f2f8691a6c3383339c304716cda8e.
    
    Change-Id: I71439d730c92836026d211be2e4b1f0514e0bcbd
    Reviewed-on: http://192.168.1.79/gerrit/9661
    Reviewed-by: b199 <b199@borqs.com>

--- drivers/gpu/drm/i915/intel_sprite.c
+++ drivers/gpu/drm/i915/intel_sprite.c
@@ -81,7 +81,7 @@ __alpha_setting_noncursor(u32 pixformat, int plane, u32 *dspcntr, int alpha)
 	case DISPPLANE_BGRX101010:
 	case DISPPLANE_RGBX161616:
 	default:
-		DRM_DEBUG("Alpha not supported for 0x%08x\n", pixformat);
+		DRM_ERROR("Unknown pixel format 0x%08x\n", pixformat);
 		break;
 	}
 }
@@ -115,11 +115,10 @@ __alpha_setting_cursor(u32 pixformat, int plane, u32 *dspcntr, int alpha)
 			*dspcntr |= CURSOR_MODE_64_32B_AX;
 		break;
 	default:
-		DRM_DEBUG("Alpha not supported for 0x%08x\n", pixformat);
+		DRM_ERROR("Unknown pixel format:Cursor 0x%08x\n", pixformat);
 		break;
 	}
 }
-
 /*
  * enable/disable alpha for planes
  */
